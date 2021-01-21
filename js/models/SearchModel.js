ff951c16a46d2891f2a993dc969c41b6f3787536
purple402 <purple402@naver.com>
2021-01-21 18:05:37 +0900

    SearchModel / add brand and fix error

--- js/models/SearchModel.js
+++ js/models/SearchModel.js
@@ -2,11 +2,13 @@ const data = [
   {
     id: 1,
     name: '2021 마이 버디 01-03 (날짜형)',
+    brand: 'DAILYLIKE',
     image: 'http://thumbnail.10x10.co.kr/webimage/image/basic600/326/B003268412.jpg?cmd=thumb&w=500&h=500&fit=true&ws=false'
   },
   {
     id: 2,
-    name: '[Peanuts] 스터디 플래너 (4종),
+    name: '[Peanuts] 스터디 플래너 (4종)',
+    brand: 'PEANUTS',
     image: 'http://thumbnail.10x10.co.kr/webimage/image/basic600/292/B002927222-4.jpg?cmd=thumb&w=500&h=500&fit=true&ws=false'
   }
 ]
