b5cc50d98cb32f233a63818d9b09d7ae3ebcb45c
purple402 <purple402@naver.com>
2021-01-26 16:52:38 +0900

    add some items

--- js/models/SearchModel.js
+++ js/models/SearchModel.js
@@ -10,6 +10,18 @@ const data = [
     name: '[Peanuts] 스터디 플래너 (4종)',
     brand: 'PEANUTS',
     image: 'http://thumbnail.10x10.co.kr/webimage/image/basic600/292/B002927222-4.jpg?cmd=thumb&w=500&h=500&fit=true&ws=false'
+  },
+  {
+    id: 3,
+    name: '라이프앤피시스 유어플래너 L 3 color',
+    brand: 'LIVEWORK',
+    image: 'http://livework.net/web/product/extra/big/202011/d48993351ce5f62812e0fd182eab98eb.jpg',
+  },
+  {
+    id: 4,
+    name: '리틀띵스 다이어리 만년형 3종',
+    brand: 'LIVEWORK',
+    image: 'http://livework.net/web/product/big/202012/75d0576c4943b345cd892feb66244ae2.jpg',
   }
 ]
 
